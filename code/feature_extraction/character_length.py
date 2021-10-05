89f73765d45f4dfee9cfd04333b88d73f7996317
fwollatz <frederik.wollatz@web.de>
2021-10-05 16:05:49 +0200

    Changes to character length.py, feature_collector.py and extract_features.py to conform with our coding conventions

--- code/feature_extraction/character_length.py
+++ code/feature_extraction/character_length.py
@@ -14,14 +14,41 @@ from code.feature_extraction.feature_extractor import FeatureExtractor
 # class for extracting the character-based length as a feature
 class CharacterLength(FeatureExtractor):
     
-    # constructor
-    def __init__(self, input_column):
+    def __init__(self, input_column: str):
+        """
+        constructor
+
+        Parameters
+        ----------
+        input_column : str
+            name of the input column
+
+        Returns
+        -------
+        None.
+
+        """
+
         super().__init__([input_column], "{0}_charlength".format(input_column))
     
     # don't need to fit, so don't overwrite _set_variables()
     
-    # compute the word length based on the inputs
-    def _get_values(self, inputs):
+
+    def _get_values(self, inputs: list) -> np.ndarray :
+        """
+        compute the word length based on the inputs
+        
+        Parameters
+        ----------
+        inputs : list
+            list of all tweets
+
+        Returns
+        -------
+        result : np.ndarray
+            array with the character-length of the individual tweets
+
+        """
         
         result = np.array(inputs[0].str.len())
         result = result.reshape(-1,1)
